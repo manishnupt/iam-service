520422f7804e1f9a6964015fa552018a8052ecd5
Manish.mehts <manish.mehts@mynxsoftwares.com>
2025-12-31 07:23:08 -0600

    token response improved

--- src/main/java/com/hrms/iam_service/dto/TokenResponse.java
+++ src/main/java/com/hrms/iam_service/dto/TokenResponse.java
@@ -1,9 +1,13 @@
 package com.hrms.iam_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class TokenResponse {
+    @JsonProperty("access_token")
     private String accessToken;
+
+    @JsonProperty("refresh_token")
     private String refreshToken;
 }
